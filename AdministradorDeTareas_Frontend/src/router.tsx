151b4ff8365d84b0f700519fcdea2752f6707d90
shogunDevOne <boris.vasquezg@utem.cl>
2024-06-14 00:24:50 -0400

    Perfil de usuario

--- AdministradorDeTareas_Frontend/src/router.tsx
+++ AdministradorDeTareas_Frontend/src/router.tsx
@@ -12,6 +12,9 @@ import RequestNewCodeView from "./views/auth/RequestNewCodeView";
 import ForgotPasswordView from "./views/auth/ForgotPasswordView";
 import NewPasswordView from "./views/auth/NewPasswordView";
 import ProjectTeamView from "./views/projects/ProjectTeamView";
+import ProfileView from "./views/profile/ProfileView";
+import ChangePasswordView from "./views/profile/ChangePasswordView";
+import ProfileLayout from "./layouts/ProfileLayout";
 
 export default function Router() {
   return (
@@ -34,6 +37,13 @@ export default function Router() {
             path="/projects/:projectId/team"
             element={<ProjectTeamView />}
           />
+          <Route element={<ProfileLayout />}>
+            <Route path="/profile" element={<ProfileView />}></Route>
+            <Route
+              path="/profile/password"
+              element={<ChangePasswordView />}
+            ></Route>
+          </Route>
         </Route>
 
         <Route element={<AuthLayout />}>
